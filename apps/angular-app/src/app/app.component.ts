a26cede261c13ef002c53ac74f1b470d9086b388
marvelmiles <marvellousabidemi2@gmail.com>
2024-10-14 14:30:05 +0100

    tryiing vercel

--- apps/angular-app/src/app/app.component.ts
+++ apps/angular-app/src/app/app.component.ts
@@ -6,23 +6,24 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { AppChartFormComponent } from './charts-form/app-chart-form.component';
-import { IChartData } from '@libs/components/ReactDataVisualizer/react-visualizer';
+// import { AppChartFormComponent } from './charts-form/app-chart-form.component';
+// import { IChartData } from '@libs/components/ReactDataVisualizer/react-visualizer';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, AppChartFormComponent],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-  @ViewChild('reactVisualizer', { static: true }) reactVisualizer!: ElementRef;
+export class AppComponent {}
+// export class AppComponent {
+//   @ViewChild('reactVisualizer', { static: true }) reactVisualizer!: ElementRef;
 
-  onChartDataReceived(data: IChartData) {
-    const visualizerElem = this.reactVisualizer.nativeElement as any;
-    visualizerElem.chartData = data;
-  }
-}
+//   onChartDataReceived(data: IChartData) {
+//     const visualizerElem = this.reactVisualizer.nativeElement as any;
+//     visualizerElem.chartData = data;
+//   }
+// }
