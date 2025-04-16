29d9b190ec9e04cfd0518541e31112bf9dfcf7bc
marvelmiles <marvellousabidemi2@gmail.com>
2025-04-16 06:04:17 +0100

    testing

--- apps/angular-app/src/main.ts
+++ apps/angular-app/src/main.ts
@@ -1,7 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import '../../../libs/components/ReactDataVisualizer/react-visualizer';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
